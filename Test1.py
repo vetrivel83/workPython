ff72b7cb11d3bef6dcf086e69e7de384d4c96c46
vetrivel83 <vetrivel83@gmail.com>
2026-09-15 13:32:14 +0530

    commit 2

--- Test1.py
+++ Test1.py
@@ -1,6 +1,8 @@
 try:
+    
 	number = float(input("Enter a number: "))
 	print(f"The square of {number} is {number ** 2}")
+    print(f"The cube of {number} is {number ** 3}")    
 except ValueError:
 	print("Please enter a valid number.")
 
---
+++ Test2.py
@@ -0,0 +1,4 @@
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
+print("Sum:", first_number + second_number)
